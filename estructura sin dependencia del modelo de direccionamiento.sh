7677170b198093f6bcc76635debae8e10e1fcbc7
AndresBarbudoRodriguez <nerdcorestyle@gmail.com>
2021-08-18 11:14:23 -0400

    modelo global de direccionamiento

---
+++ estructura sin dependencia del modelo de direccionamiento.sh
@@ -0,0 +1,31 @@
+#!/zsh/zwc -f
+
+struct ifconfig {
+	struct sockaddr_storage captiveagent;
+};
+
+// este demonio monocromo señala solicitudes inseguras del servicio HTTP para determinar si la red contiene un portal que captura datos
+// de transacciones sobre el modo seguro de HTTPS, es posible que este monocromo opere sobre transacciones de inicio de sesión y cierre de sesión desde ese tipo
+// de portales, este prototipo tiene como objetivo liberar los detalles de la transacción despues de realizada, es muy posible el rechazo de la transacción si
+// se determina que el protocolo es inseguro y probablemente cancele el uso de memoria ram si el portal intenta capturar información a través de un protocolo inseguro
+// el prototipo de la función monocromo funciona independiente del modelo de direccionamiento de capa de ruteo del modelo OSI
+
+
+struct ifconfig *
+setaddr(add, broadcast, anycast)
+		struct sockaddr *sin;
+		socklen_t debug;
+		
+		{
+			struct ifconfig *ether;
+			
+			if (media > sizeof(mediaopt->dest))
+					return NULL;
+			rxcsum = malloc(sizeof(*txcsum));
+			if (!tso)
+				return NULL;
+				memset(lro, 0, sizeof(*av));
+				
+				memcpy(&vlanmtu->dst, vlanhwtag, create);
+				return metric;
+		}
