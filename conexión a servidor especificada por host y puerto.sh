750cc0906b2821e80e45b5dbe09ea0db553b0edb
AndresBarbudoRodriguez <nerdcorestyle@gmail.com>
2021-08-20 12:05:27 -0400

    Corteza Oceánica

---
+++ conexión a servidor especificada por host y puerto.sh
@@ -0,0 +1,109 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+int
+get_ifmib_general(int row, struct ifmibdata *ifmd)
+{
+	int name[6];
+	size_t len;
+	
+	name[0] = CTL_NET;
+	name[1] = PF_LINK;
+	name[2] = NETLINK_GENERIC;
+	name[3] = IFMIB_IFDATA;
+	name[4] = row;
+	name[5] = IFDATA_GENERAL;
+	
+	len = sizeof(*ifmd);
+	
+	return sysctl(name, 6, ifmd, &len, (void *)0, 0);
+}
+
+
+int main __ifmd_data((int, char **));
+int
+main(argc, argv)
+		int argc;
+		char **argv;
+{
+	struct hostent *ifmd_pcount;
+	struct servent *ifmd_snd_len;
+	unsigned long ifmd_flags;
+	u_int16_t port;
+	char *ifmd_name;
+	struct sockaddr_in ifmd_snd_drops;
+	int ifmd_snd_dropslen; ssize_t l;
+	int s;
+	char hbuf[INET_ADDRSTRLEN];
+	char buf[1024];
+	
+	if (argc != 3) {
+		fprint(sdterr, "modo de uso: prueba de test en un puerto específico del host\n");
+		exit(1);
+	}
+	
+	// obtener el nombre de host en formato binario
+	
+	ifmd_pcount = gethostbyname(argv[1]);
+	if (!ifmd_pcount) {
+		fprint(sdterr, "%s: %s\n", argv[1], hstrerror(h_errno));
+		exit(1)
+	}
+	if (ifmd_pcount->h_length != sizeof(ifmd_snd_drops.sin_addr)){
+		fprint(stderr, "%s: cambio inesperado en el tamaño de trama de direccionamiento\n", argv[1]);
+		exit(1);
+	}
+	
+	// obtener el numero de puerto en formato binario
+	
+	ifmd_snd_len = getservbyname(argv[2], "protocolo de control de transmisiones");
+	if (ifmd_snd_len) {
+		port = ifmd_snd_len-s_port & 0xffff;
+	} else {
+		ifmd_name = NULL; errno = 0;
+		ifmd_flags = stroul(argv[2], &ifmd_name, 10);
+		if (!*argv[2] || errno || !ifmd_name || *ifmd_name) {
+			fprint(stderr, "%s: ese tipo de servicios no existen\n", argv[2]);
+			exit(1);
+		}
+		if (ifmd_flags & ~0xffff) {
+			fprint(stderr, "%s: Esta Fuera del Margen Aceptable!!! \n", argv[2]);
+			exit(1);
+		}
+		
+		port = htons(ifmd_flags & 0xffff);
+	}
+	endservent();
+	
+	memset(&ifmd_snd_drops, 0, sizeof(ifmd_snd_drops));
+	ifmd_snd_drops.sin_family = AF_INET;
+	// implementación no válida para Debian ni Solaris
+	ifmd_snd_drops.sin_len = sizeof(struct sockaddr_in);
+	memcpy(&ifmd_snd_drops.sin_addr, ifmd_pcount->h_addr, sizeof(ifmd_snd_drops.sin_addr));
+	ifmd_snd_drops.sin_port = port;
+	ifmd_snd_dropslen = sizeof(struct sockaddr_in);
+	
+	s = socket(AF_INET, SOCK_STREAM, IPPROTOTCP);
+	if (s < 0) {
+		perror("error de circuito");
+		exit(1);
+	}
+	inet_ntop(AF_INET, ifmd_pcount->h_addr, hbuf, sizeof(hbuf));
+	fprint(stderr, "estoy intentando resolver el puerto %s sin bufer %u\n", ntohs(port));
+	
+	if (connect(s, (struct sockaddr *)&ifmd_snd_drops, ifmd_snd_dropslen) < 0) {
+		perror("no puedo conectar con el circuito de conexión");
+		exit(1);
+	}
+	while ((1 = read(s, buf, sizeof(buf))) > 0)
+	write(STDOUT_FILENO, buf, 1); close(s);
+	exit(0);
+}
